d03fbf9f55816b7cb80add76ddeb96d01ed7104c
Carter Stambaugh <4051cman@gmail.com>
2019-05-25 04:53:52 -0700

    GAME

---
+++ GAMETests/GAMETests.swift
@@ -0,0 +1,34 @@
+//
+//  GAMETests.swift
+//  GAMETests
+//
+//  Created by Carter Stambaugh on 5/25/19.
+//  Copyright © 2019 JCAPP. All rights reserved.
+//
+
+import XCTest
+@testable import GAME
+
+class GAMETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
